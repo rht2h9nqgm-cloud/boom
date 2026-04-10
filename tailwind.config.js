6ee5fba83790fd81db6ee951a8c6eb5afed6bff1
rht2h9nqgm-cloud <rht2h9nqgm@privaterelay.appleid.com>
2026-04-10 05:33:36 +0100

    Add Tailwind CSS configuration with custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,18 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        outlook: {
+          blue: '#0078d4',
+          light: '#f3f2f1',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
 
